f0deafc004f70741877f04eb9fd04f1182908a43
Reinaldo Nani <reinaldo@mimobr.com>
2021-07-04 17:50:51 -0300

    start code

--- test-2xs/src/App.js
+++ test-2xs/src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          !"!!! Pushhhhh"
+          auqi!!!
+          
         </p>
         <a
           className="App-link"
